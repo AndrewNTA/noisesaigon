07218c6338aebecece5cf2beb2d568901707cd04
andrew nguyen <andrew.nta87@gmail.com>
2023-03-05 09:27:44 +0700

    Make some noise

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,8 @@
 import React, { useCallback, useMemo } from "react";
 import { Container, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import { useQuery, gql } from "@apollo/client";
 import Bg from "static/images/bg_1.png";
-import Thumbnail from "static/images/thumbnail.png";
 import {
   Spacing,
   Article,
@@ -76,14 +74,6 @@ function Home() {
 
   return (
     <Container maxWidth="lg">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Noise Saigon</title>
-        <link rel="canonical" href="http://noisesaigon.com/" />
-        <meta name="description" content="Make some noise" />
-        <meta property="og:title" content="Noise Saigon" />
-        <meta property="og:image" content={Thumbnail} />
-      </Helmet>
       <Menu />
       <img src={Bg} alt="bg" className={classes.bg} />
       <div className={classes.main}>
