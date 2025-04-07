1c546d41b1a92b687e87d1d9324046cef6e48950
andrew nguyen <andrew.nta87@gmail.com>
2025-04-07 07:49:50 +0700

    update link sp

--- src/components/SpotifyIframe.js
+++ src/components/SpotifyIframe.js
@@ -6,7 +6,7 @@ function SpotifyIframe() {
       <iframe
         title="play-list"
         style={{ borderRadius: '12px' }}
-        src="https://open.spotify.com/embed/playlist/4vwLVjilj4mSyVbjCuggrl?si=Gq9DfBZPS86XVekAmUvJ8w&pi=faGSWphuTDOiO?utm_source=generator&theme=0"
+        src="https://open.spotify.com/embed/playlist/5iQbo8qiLvYbR4b9UMHHCY?si=_aQxsyfrRD2MTLRHSozCSg&pi=faGSWphuTDOiO?utm_source=generator&theme=0"
         width="100%"
         height="152"
         frameBorder="0"
