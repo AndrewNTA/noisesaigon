1339d8cf98110472276c6457b58939b1b451a6d3
andrew nguyen <andrew.nta87@gmail.com>
2025-07-05 18:28:13 +0700

    [andrew] update spotify

--- app/components/SpotifyIframe.js
+++ app/components/SpotifyIframe.js
@@ -6,7 +6,7 @@ function SpotifyIframe() {
       <iframe
         title="play-list"
         style={{ borderRadius: '12px' }}
-        src="https://open.spotify.com/embed/playlist/5iQbo8qiLvYbR4b9UMHHCY?si=_aQxsyfrRD2MTLRHSozCSg&pi=faGSWphuTDOiO?utm_source=generator&theme=0"
+        src="https://open.spotify.com/embed/playlist/2yzLRjB2PRRpS8vZo0d9K0?si=5B8lrrGWTYyAxUlsCOzgYQ&pi=faGSWphuTDOiO?utm_source=generator&theme=0"
         width="100%"
         height="152"
         frameBorder="0"
